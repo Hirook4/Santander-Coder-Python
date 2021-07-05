f21cf7b3614044793c6bbcbcf73f291d85bb48a8
Leonardo Hirooka <leonardohirooka@hotmail.com>
2021-07-05 02:55:46 -0300

    First Commit

---
+++ Basics/2_operadores.py
@@ -0,0 +1,40 @@
+print("Operadores")
+a = 2 + 3  # Soma
+b = 5 - 2  # Subtração
+c = 2 * 3  # Multiplicação
+d = 10 / 3  # Divisão
+e = 10 // 3  # Divisão inteira
+f = 10 % 3  # Resto de divisão
+g = 3 ** 3  # Potência
+
+print(a, b, c, d, e, f, g, "\n")
+
+
+print("Operações dentro do print")
+print(a + 1, b + 1, "\n")
+
+print("Operações com Variáveis não Numéricas")
+primeiroNome = "Leonardo"
+sobrenome = " de Souza"
+print(primeiroNome + sobrenome, "\n")
+
+
+print("Operadores Relacionais")
+# Maior que: >
+# Maior ou igual: >=
+# Menor que: <
+# Menor ou igual: <=
+# Igual: ==
+# Diferente: !=
+comparacao1 = 5 > 3
+print(comparacao1)
+
+comparacao2 = 5 < 3
+print(comparacao2, "\n")
+
+print("Operadores Lógicos - and / or")
+print(5 > 3 and 6 > 3)
+print(5 < 3 and 6 > 3, "\n")
+
+print(5 > 3 or 6 > 3)
+print(5 < 3 or 6 > 3, "\n")
